179ddbc8b8ef09e666c3b2eb7b6d324a5665405f
IvkinPhilip <zloivoinivkin@gmaail.com>
2021-07-28 18:05:03 +0300

    fix model usage

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -1,3 +1,6 @@
+import ru.fivkin.credit.dto.CreditInformation;
+import ru.fivkin.credit.dto.OutInformationAboutCredit;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -5,61 +8,31 @@ import java.util.List;
 
 public class Calculation {
 
-    String listDayPayment;
-    double listMonthlyRate;
-    double listMonthlyPayment;
-    double listPercent;
-    double listRepaymentAmount;
-
-    public Calculation(String listDayPayment, double listMonthlyRate, double listMonthlyPayment, double listPercent, double listRepaymentAmount) {
-        this.listDayPayment = listDayPayment;
-        this.listMonthlyRate = listMonthlyRate;
-        this.listMonthlyPayment = listMonthlyPayment;
-        this.listPercent = listPercent;
-        this.listRepaymentAmount = listRepaymentAmount;
-    }
-
-    @Override
-    public String toString() {
-        return
-                " DayPayment: " + listDayPayment +
-                        " MonthlyRate: " + listMonthlyRate +
-                        " MonthlyPayment: " + listMonthlyPayment +
-                        " Percent: " + listPercent +
-                        " RepaymentAmount: " + listRepaymentAmount;
-    }
-
-    public Calculation() {
-
-    }
-
-    public boolean checkDoubleZero(Double checkData) {
+    public static boolean checkDoubleZero(Double checkData) {
         return checkData <= 0;
     }
 
-    public boolean checkIntZero(Integer checkData) {
+    public static boolean checkIntZero(Integer checkData) {
         return checkData <= 0;
     }
 
-    public List<Calculation> getAnnuityCalculation(double sumCredit, double interestRate, int loanPeriod, Calendar dateCredit) {
-        List<Calculation> listCalculation = new ArrayList<>();
+    public static List<OutInformationAboutCredit> getAnnuityCalculation(CreditInformation creditInformation) {
+        List<OutInformationAboutCredit> listCalculation = new ArrayList<>();
+        OutInformationAboutCredit outInformationAboutCredit = new OutInformationAboutCredit();
         try {
-            if (!checkDoubleZero(sumCredit) && !checkDoubleZero(interestRate) && !checkIntZero(loanPeriod)) {
+            if (!checkDoubleZero(creditInformation.getSumCredit()) && !checkDoubleZero(creditInformation.getInterestRate()) && !checkIntZero(creditInformation.getLoanPeriod())) {
 
-                for (int i = 0; i < loanPeriod; i++) {
-                    double monthlyRate = interestRate / (12 * 100);
-                    double monthlyPayment = sumCredit * (monthlyRate + (monthlyRate) / (Math.pow(1 + monthlyRate, 12 - i) - 1));
-                    double percent = sumCredit * monthlyRate;
-                    double repaymentAmount = monthlyPayment - percent;
+                for (int i = 0; i < creditInformation.getLoanPeriod(); i++) {
+                    double monthlyRate = (double) creditInformation.getLoanPeriod() / (12 * 100);
+                    outInformationAboutCredit.setMonthlyPayment(creditInformation.getSumCredit() * (monthlyRate + (monthlyRate) / (Math.pow(1 + monthlyRate, 12 - i) - 1)));
+                    outInformationAboutCredit.setPercent(creditInformation.getSumCredit() * monthlyRate);
+                    outInformationAboutCredit.setRepaymentAmount(outInformationAboutCredit.getMonthlyPayment() - outInformationAboutCredit.getPercent());
 
-                    dateCredit.add(Calendar.MONTH, 1);
+                    creditInformation.setDateCredit(creditInformation.getDateCredit().plusMonths(1));
 
-                    SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
-                    String formatted = formatDate.format(dateCredit.getTime());
+                    listCalculation.add(outInformationAboutCredit);
 
-                    listCalculation.add(new Calculation(formatted, monthlyPayment, repaymentAmount, percent, sumCredit));
-
-                    sumCredit -= repaymentAmount;
+                    creditInformation.setSumCredit(creditInformation.getSumCredit() - outInformationAboutCredit.getRepaymentAmount());
                 }
 
             }
@@ -73,25 +46,31 @@ public class Calculation {
         }
     }
 
-    public List<Calculation> getDiffCalculation(double sumCredit, double interestRate, int loanPeriod, Calendar dateCredit) {
-        ArrayList<Calculation> listCalculation = new ArrayList<>();
-        double repaymentAmount = sumCredit / loanPeriod;
+    public static List<OutInformationAboutCredit> getDiffCalculation(CreditInformation creditInformation) {
+        List<OutInformationAboutCredit> listCalculation = new ArrayList<>();
+        OutInformationAboutCredit outInformationAboutCredit = new OutInformationAboutCredit();
+        try {
+            outInformationAboutCredit.setRepaymentAmount(creditInformation.getSumCredit() / creditInformation.getLoanPeriod());
 
-        for (int i = 0; i < loanPeriod; i++) {
-            double monthlyRate = interestRate / (12 * 100);
-            double percent = sumCredit * monthlyRate;
-            double monthlyPayment = repaymentAmount + percent;
+            for (int i = 0; i < creditInformation.getLoanPeriod(); i++) {
+                double monthlyRate = creditInformation.getInterestRate() / (12 * 100);
+                outInformationAboutCredit.setPercent(creditInformation.getSumCredit() * monthlyRate);
+                outInformationAboutCredit.setMonthlyPayment(outInformationAboutCredit.getRepaymentAmount() + outInformationAboutCredit.getPercent());
 
-            dateCredit.add(Calendar.MONTH, 1);
+                creditInformation.setDateCredit(creditInformation.getDateCredit().plusMonths(1));
 
-            SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
-            String formatted = formatDate.format(dateCredit.getTime());
+                listCalculation.add(outInformationAboutCredit);
 
-            listCalculation.add(new Calculation(formatted, monthlyPayment, repaymentAmount, percent, sumCredit));
+                creditInformation.setSumCredit(creditInformation.getSumCredit() - outInformationAboutCredit.getRepaymentAmount());
+            }
 
-            sumCredit -= repaymentAmount;
+            return listCalculation;
+        } catch (NullPointerException e) {
+            System.err.println("Exception NullPointer");
+            return listCalculation;
+        } catch (ArithmeticException e) {
+            System.err.println("Exception ArithmeticException");
+            return listCalculation;
         }
-
-        return listCalculation;
     }
 }
--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -1,47 +1,290 @@
-import java.util.*;
+import org.junit.jupiter.api.Test;
+import ru.fivkin.credit.dto.CreditInformation;
+import ru.fivkin.credit.dto.OutInformationAboutCredit;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculationTest {
-    public static void main(String[] args) {
-        testGetAnnuityCalculation();
+    @Test
+    void testNullSumCreditAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(null);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
     }
 
-    private static void testGetAnnuityCalculation() {
-        Calculation cal = new Calculation();
-        Calendar calendar = new GregorianCalendar(2017, 0 , 25);
+    @Test
+    void testNullInterestRateAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(null);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
 
-        List<Calculation> testList = cal.getAnnuityCalculation(30000, 17.5, 12, null);
-        List<Calculation> testResultList = new ArrayList<>();
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
 
-        Assert.assertEquals("Test List if data is null",testList,testResultList);
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
 
-        List<Calculation> testListZeroFirst = cal.getAnnuityCalculation(0, 17.5, 12, calendar);
-        List<Calculation> testResultListZeroFirst = new ArrayList<>();
+    @Test
+    void testNullLoanPeriodAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(null);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
 
-        Assert.assertEquals("Test List if sum is 0",testListZeroFirst,testResultListZeroFirst);
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
 
-        List<Calculation> testListZeroSecond = cal.getAnnuityCalculation(30000, 0, 12, calendar);
-        List<Calculation> testResultListZeroSecond = new ArrayList<>();
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
 
-        Assert.assertEquals("Test List if rate is 0",testListZeroSecond,testResultListZeroSecond);
+    @Test
+    void testNullDateCreditAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        creditInformation.setDateCredit(null);
 
-        List<Calculation> testListZeroThird = cal.getAnnuityCalculation(30000, 17.5, 0, calendar);
-        List<Calculation> testResultListZeroThird = new ArrayList<>();
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
 
-        Assert.assertEquals("Test List if loanPeriod is 0", testListZeroThird,testResultListZeroThird);
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
 
-        List<Calculation> testListMinusFirst = cal.getAnnuityCalculation(-1, 17.5, 12, calendar);
-        List<Calculation> testResultListMinusFirst = new ArrayList<>();
+    @Test
+    void testZeroSumCreditAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(0.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
 
-        Assert.assertEquals("Test List if sum is -1",testListMinusFirst,testResultListMinusFirst);
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
 
-        List<Calculation> testListMinusSecond = cal.getAnnuityCalculation(30000, -1, 12, calendar);
-        List<Calculation> testResultListMinusSecond = new ArrayList<>();
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
 
-        Assert.assertEquals("Test List if rate is -1",testListMinusSecond,testResultListMinusSecond);
+    @Test
+    void testZeroInterestRateAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(0.0);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
 
-        List<Calculation> testListMinusThird = cal.getAnnuityCalculation(30000, 17.5, -1, calendar);
-        List<Calculation> testResultListMinusThird = new ArrayList<>();
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
 
-        Assert.assertEquals("Test List if loanPeriod is -1", testListMinusThird,testResultListMinusThird);
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testZeroLoanPeriodAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(0);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testMinusSumCreditAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(0.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testMinusInterestRateAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(0.0);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testMinusLoanPeriodAnnuityCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(0);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testNullSumCreditDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(null);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testNullInterestRateDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(null);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testNullLoanPeriodDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(null);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testNullDateCreditDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        creditInformation.setDateCredit(null);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testZeroSumCreditDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(0.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testZeroInterestRateDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(0.0);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testZeroLoanPeriodDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(0);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testMinusSumCreditDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(0.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testMinusInterestRateDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(0.0);
+        creditInformation.setLoanPeriod(12);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
+    }
+
+    @Test
+    void testMinusLoanPeriodDiffCalculation() {
+        CreditInformation creditInformation = new CreditInformation();
+        creditInformation.setSumCredit(30000.0);
+        creditInformation.setInterestRate(17.5);
+        creditInformation.setLoanPeriod(0);
+        LocalDate dateCredit = LocalDate.of(2017, Month.NOVEMBER, 30);
+        creditInformation.setDateCredit(dateCredit);
+
+        List<OutInformationAboutCredit> testListCalculation = new ArrayList<>();
+
+        assertEquals(Calculation.getAnnuityCalculation(creditInformation), testListCalculation);
     }
 }
