c77233556f001f063cea5f303c74e63e5f3577a4
IvkinPhilip <zloivoinivkin@gmaail.com>
2021-08-06 16:48:21 +0300

    fix tests

--- src/main/java/ru/fivkin/Calculation.java
+++ src/main/java/ru/fivkin/Calculation.java
@@ -5,13 +5,14 @@ import ru.fivkin.credit.dto.CreditInformation;
 import ru.fivkin.credit.dto.OutInformationAboutCredit;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 public class Calculation {
 
-    private Calculation(){
+    private Calculation() {
 
     }
 
@@ -32,22 +33,21 @@ public class Calculation {
         List<OutInformationAboutCredit> listCalculation = new ArrayList<>();
         try {
             if (checkBigDecimalZero(creditInformation.getSumCredit()) > 0 && checkDoubleZero(creditInformation.getInterestRate()) && checkIntZero(creditInformation.getLoanPeriod())) {
-
                 for (int i = 0; i < creditInformation.getLoanPeriod(); i++) {
                     OutInformationAboutCredit outInformationAboutCredit = new OutInformationAboutCredit();
                     double monthlyRate = creditInformation.getInterestRate() / (12 * 100);
-                    outInformationAboutCredit.setMonthlyPayment(creditInformation.getSumCredit().toBigInteger().doubleValue() * (monthlyRate + ((monthlyRate) / (Math.pow(1 + monthlyRate, 12.0 - i) - 1))));
-                    outInformationAboutCredit.setPercent(creditInformation.getSumCredit().toBigInteger().doubleValue() * monthlyRate);
-                    outInformationAboutCredit.setRepaymentAmount(outInformationAboutCredit.getMonthlyPayment() - outInformationAboutCredit.getPercent());
+                    outInformationAboutCredit.setMonthlyPayment(creditInformation.getSumCredit().multiply(BigDecimal.valueOf(monthlyRate + ((monthlyRate) / (Math.pow(1 + monthlyRate, 12.0 - i) - 1)))).setScale(13, RoundingMode.HALF_DOWN));
+                    outInformationAboutCredit.setPercent(creditInformation.getSumCredit().multiply(BigDecimal.valueOf(monthlyRate)).setScale(12, RoundingMode.HALF_DOWN));
+                    outInformationAboutCredit.setRepaymentAmount(outInformationAboutCredit.getMonthlyPayment().subtract(outInformationAboutCredit.getPercent()).setScale(13, RoundingMode.HALF_DOWN));
 
                     creditInformation.setDateCredit(creditInformation.getDateCredit().plusMonths(1));
                     outInformationAboutCredit.setDatePayment(creditInformation.getDateCredit());
 
-                    outInformationAboutCredit.setSumCredit(creditInformation.getSumCredit());
+                    outInformationAboutCredit.setSumCredit(creditInformation.getSumCredit().setScale(13, RoundingMode.HALF_DOWN));
 
                     listCalculation.add(outInformationAboutCredit);
 
-                    creditInformation.setSumCredit(creditInformation.getSumCredit().subtract(BigDecimal.valueOf(outInformationAboutCredit.getRepaymentAmount())));
+                    creditInformation.setSumCredit(creditInformation.getSumCredit().subtract(outInformationAboutCredit.getRepaymentAmount()).setScale(13, RoundingMode.HALF_DOWN));
                 }
 
             }
@@ -60,18 +60,16 @@ public class Calculation {
 
     public static List<OutInformationAboutCredit> getDiffCalculation(CreditInformation creditInformation) {
         List<OutInformationAboutCredit> listCalculation = new ArrayList<>();
-
-
         try {
             if (checkBigDecimalZero(creditInformation.getSumCredit()) > 0 && checkDoubleZero(creditInformation.getInterestRate()) && checkIntZero(creditInformation.getLoanPeriod())) {
 
-                double repayment = creditInformation.getSumCredit().toBigInteger().doubleValue() / creditInformation.getLoanPeriod();
+                BigDecimal repayment = creditInformation.getSumCredit().divide(BigDecimal.valueOf(creditInformation.getLoanPeriod())) ;
                 for (int i = 0; i < creditInformation.getLoanPeriod(); i++) {
                     OutInformationAboutCredit outInformationAboutCredit = new OutInformationAboutCredit();
                     outInformationAboutCredit.setRepaymentAmount(repayment);
                     double monthlyRate = creditInformation.getInterestRate() / (12 * 100);
-                    outInformationAboutCredit.setPercent(creditInformation.getSumCredit().toBigInteger().doubleValue() * monthlyRate);
-                    outInformationAboutCredit.setMonthlyPayment(outInformationAboutCredit.getRepaymentAmount() + outInformationAboutCredit.getPercent());
+                    outInformationAboutCredit.setPercent(creditInformation.getSumCredit().multiply(BigDecimal.valueOf(monthlyRate)).setScale(13, RoundingMode.HALF_DOWN));
+                    outInformationAboutCredit.setMonthlyPayment(outInformationAboutCredit.getRepaymentAmount().add(outInformationAboutCredit.getPercent()).setScale(13, RoundingMode.HALF_DOWN));
 
 
                     creditInformation.setDateCredit(creditInformation.getDateCredit().plusMonths(1));
@@ -81,7 +79,7 @@ public class Calculation {
 
                     listCalculation.add(outInformationAboutCredit);
 
-                    creditInformation.setSumCredit(creditInformation.getSumCredit().subtract(BigDecimal.valueOf(outInformationAboutCredit.getRepaymentAmount())));
+                    creditInformation.setSumCredit(creditInformation.getSumCredit().subtract(outInformationAboutCredit.getRepaymentAmount()).setScale(13, RoundingMode.HALF_DOWN));
                 }
             }
 
--- src/test/java/ru/fivkin/CalculationTest.java
+++ src/test/java/ru/fivkin/CalculationTest.java
@@ -23,35 +23,47 @@ class CalculationTest {
 
     @Test
     void testAnnuityCalculation() {
-        List<OutInformationAboutCredit> testList = getAnnuityCalculation(new CreditInformation().sumCredit(BigDecimal.
-                valueOf(30000)).loanPeriod(12).interestRate(17.5).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 30)));
+        List<OutInformationAboutCredit> testList = getAnnuityCalculation(new CreditInformation().sumCredit((BigDecimal.
+                valueOf(30000))).loanPeriod(12).interestRate(17.5).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 30)));
         ArrayList<OutInformationAboutCredit> testAnnuityResult = new ArrayList<>();
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2017, Month.DECEMBER, 30)).
-                monthlyPayment(2743.2661289420766).repaymentAmount(2305.7661289420766).percent(437.5).sumCredit(new BigDecimal("30000")));
+                monthlyPayment(new BigDecimal("2743.2661289420766")).repaymentAmount(new BigDecimal("2305.7661289420766")).
+                percent(new BigDecimal("437.500000000000")).sumCredit(new BigDecimal("30000.0000000000000")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.JANUARY, 30)).
-                monthlyPayment(2743.2429627279585).repaymentAmount(2339.372129394625).percent(403.87083333333334).sumCredit(new BigDecimal("27694.2338710579234")));
+                monthlyPayment(new BigDecimal("2743.2661289420783")).repaymentAmount(new BigDecimal("2339.3918849891503")).
+                percent(new BigDecimal("403.874243952928")).sumCredit(new BigDecimal("27694.2338710579234")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.FEBRUARY, 28)).
-                monthlyPayment(2743.175030292567).repaymentAmount(2373.4291969592336).percent(369.74583333333334).sumCredit(new BigDecimal("25354.8617416632984")));
+                monthlyPayment(new BigDecimal("2743.2661289420784")).repaymentAmount(new BigDecimal("2373.5080166452424")).
+                percent(new BigDecimal("369.758112296836")).sumCredit(new BigDecimal("25354.8419860687731")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.MARCH, 28)).
-                monthlyPayment(2743.2262632402485).repaymentAmount(2408.0866799069154).percent(335.13958333333335).sumCredit(new BigDecimal("22981.4325447040648")));
+                monthlyPayment(new BigDecimal("2743.2661289420782")).repaymentAmount(new BigDecimal("2408.1216752213182")).
+                percent(new BigDecimal("335.144453720760")).sumCredit(new BigDecimal("22981.3339694235307")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.APRIL, 28)).
-                monthlyPayment(2743.2378212822982).repaymentAmount(2443.2149046156314).percent(300.0229166666667).sumCredit(new BigDecimal("20573.3458647971494")));
+                monthlyPayment(new BigDecimal("2743.2661289420829")).repaymentAmount(new BigDecimal("2443.2401163183009")).
+                percent(new BigDecimal("300.026012623782")).sumCredit(new BigDecimal("20573.2122942022125")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.MAY, 28)).
-                monthlyPayment(2743.270338737217).repaymentAmount(2478.8745054038836).percent(264.3958333333333).sumCredit(new BigDecimal("18130.1309601815180")));
+                monthlyPayment(new BigDecimal("2743.2661289420803")).repaymentAmount(new BigDecimal("2478.8707013479403")).
+                percent(new BigDecimal("264.395427594140")).sumCredit(new BigDecimal("18129.9721778839116")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.JUNE, 28)).
-                monthlyPayment(2743.2483425169944).repaymentAmount(2515.0045925169943).percent(228.24375).sumCredit(new BigDecimal("15651.2564547776344")));
+                monthlyPayment(new BigDecimal("2743.2661289420858")).repaymentAmount(new BigDecimal("2515.0208990759358")).
+                percent(new BigDecimal("228.245229866150")).sumCredit(new BigDecimal("15651.1014765359713")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.JULY, 28)).
-                monthlyPayment(2743.2493015927403).repaymentAmount(2551.6826349260737).percent(191.56666666666666).sumCredit(new BigDecimal("13136.2518622606401")));
+                monthlyPayment(new BigDecimal("2743.2661289420832")).repaymentAmount(new BigDecimal("2551.6982871874572")).
+                percent(new BigDecimal("191.567841754626")).sumCredit(new BigDecimal("13136.0805774600355")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.AUGUST, 28)).
-                monthlyPayment(2743.1670467351687).repaymentAmount(2588.817046735169).percent(154.35).sumCredit(new BigDecimal("10584.5692273345664")));
+                monthlyPayment(new BigDecimal("2743.2661289420875")).repaymentAmount(new BigDecimal("2588.9105538756125")).
+                percent(new BigDecimal("154.355575066475")).sumCredit(new BigDecimal("10584.3822902725783")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.SEPTEMBER, 28)).
-                monthlyPayment(2743.1042750174875).repaymentAmount(2626.5105250174875).percent(116.59375).sumCredit(new BigDecimal("7995.7521805993974")));
+                monthlyPayment(new BigDecimal("2743.2661289420902")).repaymentAmount(new BigDecimal("2626.6654994529682")).
+                percent(new BigDecimal("116.600629489122")).sumCredit(new BigDecimal("7995.4717363969658")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.OCTOBER, 28)).
-                monthlyPayment(2743.365134867267).repaymentAmount(2665.0672182006).percent(78.29791666666667).sumCredit(new BigDecimal("5369.2416555819099")));
+                monthlyPayment(new BigDecimal("2743.2661289420923")).repaymentAmount(new BigDecimal("2664.9710379866593")).
+                percent(new BigDecimal("78.295090955433")).sumCredit(new BigDecimal("5368.8062369439976")));
         testAnnuityResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.NOVEMBER, 28)).
-                monthlyPayment(2743.4333333333225).repaymentAmount(2703.999999999989).percent(39.43333333333334).sumCredit(new BigDecimal("2704.1744373813099")));
+                monthlyPayment(new BigDecimal("2743.2661289421222")).repaymentAmount(new BigDecimal("2703.8351989573272")).
+                percent(new BigDecimal("39.430929984795")).sumCredit(new BigDecimal("2703.8351989573383")));
         for (int i = 0; i < testList.size(); i++) {
-            assertEquals(testAnnuityResult.get(i), testList.get(i));
+            assertEquals(testAnnuityResult, testList);
         }
     }
 
@@ -61,31 +73,43 @@ class CalculationTest {
                 valueOf(30000)).loanPeriod(12).interestRate(17.5).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 30)));
         ArrayList<OutInformationAboutCredit> testDiffResult = new ArrayList<>();
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2017, Month.DECEMBER, 30)).
-                monthlyPayment(2937.5).repaymentAmount(2500.0).percent(437.5).sumCredit(BigDecimal.valueOf(30000)));
+                monthlyPayment(new BigDecimal("2937.5000000000000")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("437.5000000000000")).sumCredit(new BigDecimal("30000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.JANUARY, 30)).
-                monthlyPayment(2901.0416666666665).repaymentAmount(2500.0).percent(401.0416666666667).sumCredit(BigDecimal.valueOf(27500.0)));
+                monthlyPayment(new BigDecimal("2901.0416666666667")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("401.0416666666667")).sumCredit(new BigDecimal("27500.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.FEBRUARY, 28)).
-                monthlyPayment(2864.5833333333335).repaymentAmount(2500.0).percent(364.5833333333333).sumCredit(BigDecimal.valueOf(25000.0)));
+                monthlyPayment(new BigDecimal("2864.5833333333333")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("364.5833333333333")).sumCredit(new BigDecimal("25000.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.MARCH, 28)).
-                monthlyPayment(2828.125).repaymentAmount(2500.0).percent(328.125).sumCredit(BigDecimal.valueOf(22500.0)));
+                monthlyPayment(new BigDecimal("2828.1250000000000")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("328.1250000000000")).sumCredit(new BigDecimal("22500.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.APRIL, 28)).
-                monthlyPayment(2791.6666666666665).repaymentAmount(2500.0).percent(291.6666666666667).sumCredit(BigDecimal.valueOf(20000.0)));
+                monthlyPayment(new BigDecimal("2791.6666666666667")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("291.6666666666667")).sumCredit(new BigDecimal("20000.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.MAY, 28)).
-                monthlyPayment(2755.2083333333335).repaymentAmount(2500.0).percent(255.20833333333334).sumCredit(BigDecimal.valueOf(17500.0)));
+                monthlyPayment(new BigDecimal("2755.2083333333333")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("255.2083333333333")).sumCredit(new BigDecimal("17500.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.JUNE, 28)).
-                monthlyPayment(2718.75).repaymentAmount(2500.0).percent(218.75).sumCredit(BigDecimal.valueOf(15000.0)));
+                monthlyPayment(new BigDecimal("2718.7500000000000")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("218.7500000000000")).sumCredit(new BigDecimal("15000.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.JULY, 28)).
-                monthlyPayment(2682.2916666666665).repaymentAmount(2500.0).percent(182.29166666666666).sumCredit(BigDecimal.valueOf(12500.0)));
+                monthlyPayment(new BigDecimal("2682.2916666666667")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("182.2916666666667")).sumCredit(new BigDecimal("12500.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.AUGUST, 28)).
-                monthlyPayment(2645.8333333333335).repaymentAmount(2500.0).percent(145.83333333333334).sumCredit(BigDecimal.valueOf(10000.0)));
+                monthlyPayment(new BigDecimal("2645.8333333333333")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("145.8333333333333")).sumCredit(new BigDecimal("10000.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.SEPTEMBER, 28)).
-                monthlyPayment(2609.375).repaymentAmount(2500.0).percent(109.375).sumCredit(BigDecimal.valueOf(7500.0)));
+                monthlyPayment(new BigDecimal("2609.3750000000000")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("109.3750000000000")).sumCredit(new BigDecimal("7500.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.OCTOBER, 28)).
-                monthlyPayment(2572.9166666666665).repaymentAmount(2500.0).percent(72.91666666666667).sumCredit(BigDecimal.valueOf(5000.0)));
+                monthlyPayment(new BigDecimal("2572.9166666666667")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("72.9166666666667")).sumCredit(new BigDecimal("5000.0000000000000")));
         testDiffResult.add(new OutInformationAboutCredit().datePayment(LocalDate.of(2018, Month.NOVEMBER, 28)).
-                monthlyPayment(2536.4583333333335).repaymentAmount(2500.0).percent(36.458333333333336).sumCredit(BigDecimal.valueOf(2500.0)));
+                monthlyPayment(new BigDecimal("2536.4583333333333")).repaymentAmount(new BigDecimal("2500")).
+                percent(new BigDecimal("36.4583333333333")).sumCredit(new BigDecimal("2500.0000000000000")));
         for (int i = 0; i < testList.size(); i++) {
-            assertEquals(testDiffResult.get(i), testList.get(i));
+            assertEquals(testDiffResult,testList);
         }
     }
 
@@ -94,22 +118,22 @@ class CalculationTest {
     @ParameterizedTest
     @MethodSource("provideValueSumCredit")
     void testValueSumCredit(CreditInformation creditInformation) {
-        List<OutInformationAboutCredit> testList =getAnnuityCalculation(creditInformation);
+        List<OutInformationAboutCredit> testList = getAnnuityCalculation(creditInformation);
         assertTrue(testList.isEmpty());
     }
 
     private static Stream<Arguments> provideValueSumCredit() {
         return Stream.of(
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(0)).interestRate(17.5).loanPeriod(12).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(30000)).interestRate(0.0).loanPeriod(12).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(30000)).interestRate(17.5).loanPeriod(0).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(null).interestRate(17.5).loanPeriod(12).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(30000)).interestRate(null).loanPeriod(12).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(30000)).interestRate(17.5).loanPeriod(null).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(30000)).interestRate(17.5).loanPeriod(12).dateCredit(null)),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(-1)).interestRate(17.5).loanPeriod(12).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(30000)).interestRate(-1.0).loanPeriod(12).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28))),
-                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(30000)).interestRate(17.5).loanPeriod(-1).dateCredit(LocalDate.of(2017, Month.NOVEMBER, 28)))
+                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(0))),
+                Arguments.of(creditInformation.interestRate(0.0)),
+                Arguments.of(creditInformation.loanPeriod(0)),
+                Arguments.of(creditInformation.sumCredit(null)),
+                Arguments.of(creditInformation.interestRate(null)),
+                Arguments.of(creditInformation.loanPeriod(null)),
+                Arguments.of(creditInformation.dateCredit(null)),
+                Arguments.of(creditInformation.sumCredit(BigDecimal.valueOf(-1))),
+                Arguments.of(creditInformation.interestRate(-1.0)),
+                Arguments.of(creditInformation.loanPeriod(-1))
         );
     }
 }
